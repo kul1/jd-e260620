ed73edd78faaccdbdab79ec9fb8a98de398bb9aa
kul1 <1.0@kul.asia>
2020-03-30 13:13:52 -0700

    Init

---
+++ config/routes.rb
@@ -0,0 +1,34 @@
+Rails.application.routes.draw do
+  # start jiinda method routes
+  jinda_methods = ['pending','status','search','doc','logs','ajax_notice']
+  jinda_methods += ['init','run','run_mail','document','run_do','run_form','end_form','error_logs', 'notice_logs', 'cancel']
+  jinda_methods.each do |aktion| get "/jinda/#{aktion}" => "jinda##{aktion}" end
+  post '/jinda/init' => 'jinda#init'
+  post '/jinda/pending' => 'jinda#index'
+  post '/jinda/end_form' => 'jinda#end_form'
+  # end jinda method routes
+  post '/auth/:provider/callback' => 'sessions#create'
+  get '/auth/:provider/callback' => 'sessions#create'
+  get '/auth/failure' => 'sessions#destroy'
+  get '/logout' => 'sessions#destroy', :as => 'logout'
+  get '/articles/my' => 'articles/my'
+  get '/articles/my/destroy' => 'articles#destroy'
+  get '/notes/my' => 'notes/my'
+  get '/notes/my/destroy/:id' => 'notes#destroy'
+  get '/notes/destroy/:id' => 'notes#destroy'
+  resources :articles
+  resources :notes
+  resources :users
+  resources :identities
+  resources :sessions
+  resources :password_resets
+  resources :jinda, :only => [:index, :new]
+  root :to => 'jinda#index'
+  # api
+  get '/api/v1/notes/my' => 'api/v1/notes#my'
+  post '/api/v1/notes' => 'api/v1/notes#create', as: 'api_v1_notes'
+  namespace :api do
+  namespace :v1 do resources :notes, :only => [:index] end
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
